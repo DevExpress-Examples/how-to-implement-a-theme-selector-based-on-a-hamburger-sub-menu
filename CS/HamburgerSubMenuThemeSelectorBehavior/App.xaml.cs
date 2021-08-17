bfdd89a7f4c147050f9519bdc1bbcdeae74f09b3
Anton Sameishchev <anton.sameishchev@devexpress.com>
2021-08-17 15:01:52 +0300

    Code Added

---
+++ CS/HamburgerSubMenuThemeSelectorBehavior/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.Xpf.Core;
+using System.Linq;
+using System.Windows;
+
+namespace HamburgerSubMenuThemeSelectorBehavior
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            foreach (var theme in Theme.Themes.ToList())
+            {
+                switch (theme.Category)
+                {
+                    case Theme.Office2007Category:
+                        theme.ShowInThemeSelector = false;
+                        break;
+                    case Theme.MetropolisCategory:
+                        theme.ShowInThemeSelector = false;
+                        break;
+                    default:
+                        break;
+                }
+                if (theme.Name == "DeepBlue")
+                {
+                    theme.ShowInThemeSelector = false;
+                }
+            }
+        }
+    }
+}
