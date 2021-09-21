b252724eb7a904fdcf7ac9b26deb41fb955d04be
DevExpressExampleBot <fakeEmail>
2021-09-21 15:40:32 +0300

    Source auto update [skip ci]

---
+++ VB/HamburgerSubMenuThemeSelectorBehavior/App.xaml.vb
@@ -0,0 +1,29 @@
+Imports DevExpress.Xpf.Core
+Imports System.Linq
+Imports System.Windows
+
+Namespace HamburgerSubMenuThemeSelectorBehavior
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+        Public Sub New()
+            For Each theme As Theme In Theme.Themes.ToList()
+                Select Case theme.Category
+                    Case Theme.Office2007Category
+                        theme.ShowInThemeSelector = False
+                    Case Theme.MetropolisCategory
+                        theme.ShowInThemeSelector = False
+                    Case Else
+                End Select
+
+                If theme.Name Is "DeepBlue" Then
+                    theme.ShowInThemeSelector = False
+                End If
+            Next
+        End Sub
+    End Class
+End Namespace
